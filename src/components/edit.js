f2bf683122168d75a72cce5a13612c41a3971529
Chrysalis <63181733+chrysaliswoon@users.noreply.github.com>
2022-03-31 20:08:46 +0800

    update url link

--- src/components/edit.js
+++ src/components/edit.js
@@ -53,7 +53,7 @@ export default function Edit() {
    };
  
    // This will send a post request to update the data in the database.
-   await fetch(`http://localhost:3000/update/${params.id}`, {
+   await fetch(`https://mern-employee-database.herokuapp.com//update/${params.id}`, {
      method: "POST",
      body: JSON.stringify(editedPerson),
      headers: {
